ef98da28097b3745ec3d4d837cd3e42fa8d38315
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2021-12-28 21:52:42 +0530

    validator

---
+++ validator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    bool check;
+    cout << "Enter the card number : " ;
+    cin >> n;
+    if(n<13 || n>16)
+    {
+        cout << "Enter a valid number" << endl;
+    }
+    else{
+        
+    }
+    return 0;
+}
