8ceb8323ca97de60dca73c89a1057db6d84fa801
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2021-12-29 18:28:47 +0530

    validator

--- validator.cpp
+++ validator.cpp
@@ -2,10 +2,6 @@
 #include <string>
 using namespace std;
 int main(){
-    string n;
-    cout << "Enter card number digit by digit: " << "";
-    cin >> n;
-    int size=n.length();
-    cout << size;
+
     return 0;
 }
