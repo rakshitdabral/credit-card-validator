e6b6cac5242aac1036198a73b97f236d6761efd7
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2021-12-29 21:27:48 +0530

    validator

--- validator.cpp
+++ validator.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     string number;
-    int sum_odd=0;
+    int sum_odd = 0,sum_even=0;
     cout << "Enter cc number: " << "";
     cin >> number;
 //string to array conversion
@@ -13,8 +13,23 @@ int main(){
         arr[i] = (int)(ck) - 48;
     }
 //sum of odd place digits
-    while(number.size()>0){
-        
-    }
+        for(int i=0;i<number.length();i++){
+            if(i==0){
+                continue;
+            }
+            if(i%2==1){
+                sum_odd=sum_odd+arr[i];
+            }
+        }
+//sum of even place digits
+        for(int i=0;i<number.length();i++){
+            if(i%2==0){
+                sum_even=sum_even+arr[i]*2;
+                if(sum_even>9){
+                    sum_even=sum_even%10;
+                    
+                }
+            }
+        }cout << sum_even;
     return 0;
 }
