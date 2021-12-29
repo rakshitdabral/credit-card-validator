fda3a134655335cf67bcd8467ac7b330855c3eb5
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2021-12-29 22:31:15 +0530

    validator

--- validator.cpp
+++ validator.cpp
@@ -6,30 +6,38 @@ int main(){
     int sum_odd = 0,sum_even=0;
     cout << "Enter cc number: " << "";
     cin >> number;
-//string to array conversion
     int arr[number.size()];
-    for(int i=0;i<number.length();i++){
+//string to array conversion and reversing the order
+    
+    for(int i=number.length()-1;i>=0;i--){
         char ck=(char)(number[i]);
-        arr[i] = (int)(ck) - 48;
-    }
+        arr[number.length()-1-i] = (int)(ck) - 48;
+    } 
 //sum of odd place digits
         for(int i=0;i<number.length();i++){
-            if(i==0){
-                continue;
-            }
-            if(i%2==1){
+            if(i%2==0){
                 sum_odd=sum_odd+arr[i];
             }
         }
+        
 //sum of even place digits
         for(int i=0;i<number.length();i++){
-            if(i%2==0){
-                sum_even=sum_even+arr[i]*2;
-                if(sum_even>9){
-                    sum_even=sum_even%10;
-                    
+            if(i%2==1){
+                int temp = arr[i]*2;
+                if(temp>9){
+                    temp=(temp%10)+(temp/10);
                 }
+                sum_even += temp;
             }
-        }cout << sum_even;
+        }
+        
+//checker for valid
+    int final_sum=sum_even+sum_odd;
+    if(final_sum%10==0){
+        cout << "Valid card number";
+    }
+    else{
+        cout << "Invalid card number";
+    }
     return 0;
 }
